b03b5adb84c7afc0b7089dfc3c6f29e0a8effb33
lanhatquang1998 <quangla.24hdev@gmail.com>
2020-08-31 17:28:11 +0700

    finished to add authentication to our burger project

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -17,7 +17,7 @@ const sideDrawer = (props) => {
             <div className={attachedClasses.join(' ')}>
                 <Logo height="11%" className="LogoSD"/>
                 <nav>
-                    <NavigationItems />
+                    <NavigationItems isAuthenticated={props.isAuth}/>
                 </nav>
             </div>
         </Aux>
